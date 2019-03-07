5b5a68ae66cd2fcbf3ca2dcc694c4bb00550bb4e
Kenn <kenn+git@code-crew.org>
2019-03-07 13:24:11 -0600

    update

--- InheritanceApp/urls.py
+++ InheritanceApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('comments/', views.comments, name='comments'),
+    path('details/', views.details, name='details'),
+    path('poster/', views.poster, name='poster'),
 ]
--- InheritanceApp/views.py
+++ InheritanceApp/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'InheritanceApp/index.html')
+
+
+def comments(request):
+    return render(request, 'InheritanceApp/comments.html')
+
+
+def poster(request):
+    return render(request, 'InheritanceApp/poster.html')
+
+
+def details(request):
+    return render(request, 'InheritanceApp/details.html')
